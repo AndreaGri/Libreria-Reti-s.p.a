a9630c3bc61cde48b3f3018d9e352e64eac8ba18
Andrea Grillo <grillo.andrea@iisgalvanimi.edu.it>
2024-05-17 10:56:25 +0000

    -

--- static/script.js
+++ static/script.js
@@ -1,3 +1,10 @@
 function Cerca(){
     var libro=document.getElementById('cerca')
+    console.log(libro)
+    fetch('https://librarymanagementpw.azurewebsites.net/api/Book')
+    .then(Response => Response.json)
+    .then(libri =>{
+        console.log(libri)
+        if (libro in )
+    })
 }
