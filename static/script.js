717e9a0d2a25ea35f6571afc5badcac61a1a4069
Andrea Grillo <grillo.andrea@iisgalvanimi.edu.it>
2024-05-23 07:45:33 +0000

    get

--- static/script.js
+++ static/script.js
@@ -1,10 +1,45 @@
-function Cerca(){
-    var libro=document.getElementById('cerca')
-    console.log(libro)
+function cerca() {
+    var libro = String(document.getElementById('t').value);
+    console.log(libro);
+
     fetch('https://librarymanagementpw.azurewebsites.net/api/Book')
-    .then(Response => Response.json)
-    .then(libri =>{
-        console.log(libri)
-        if (libro in )
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let found = false;
+            for (const book of data) {
+                console.log(book['title']);
+                if (book['title'] === libro) {
+                    console.log('si');
+                    console.log(book)
+                    document.getElementById('out').innerHTML = [book['title'] + '<br>' + book['id']+ '<br>' +book['genreId']+ '<br>' +book['genreName']+ '<br>' +
+                    book['price']+ '<br>' +book['shelfId']
+                ];
+                    found = true;
+                    break; // Esci dal ciclo una volta trovato il libro
+                }
+            }
+            if (!found) {
+                console.log('Libro non trovato');
+                document.getElementById('out').innerHTML = 'Libro non trovato';
+            }
+        })
+        .catch(err => alert("Qualcosa è andato storto nella richiesta"));
+}
+
+
+
+function vedi(){
+    fetch('https://librarymanagementpw.azurewebsites.net/api/Book')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      let foundBooks;
+      for (const book of data) {
+        console.log(book['title']);
+        foundBooks += book['title'] + '<br>';
+        }
+       document.getElementById('out').innerHTML= foundBooks
     })
+    .catch(err => alert("Qualcosa è andato storto nella richiesta"))
 }
